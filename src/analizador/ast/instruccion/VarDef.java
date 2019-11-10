8b50b2e53aaa897e5c07adc66f2d35738f310f9d
Oscar Morales <oskralberto14@gmail.com>
2019-11-10 02:22:23 -0600

    Agregando tipo array en var

--- src/analizador/ast/instruccion/VarDef.java
+++ src/analizador/ast/instruccion/VarDef.java
@@ -84,12 +84,93 @@ public class VarDef extends Instruccion {
                     return null;
                 }
             } else if (Tipo.getVariables() != null) {
+                Tipo.setIdRecord(this.hashCode());
                 Tipo.setEntorno(new Entorno("record", e));
                 Tipo.getVariables().forEach((variable) -> {
                     variable.GetCuadruplos(Tipo.getEntorno(), errores, global);
                 });
                 Tipo.getEntorno().setSize(Tipo.getEntorno().getPos());
                 Tipo.getEntorno().setPadre(null);
+            } else if (Tipo.getDimensiones() != null) {
+
+                while (Tipo.getTipoArray().IsArray()) {
+                    Tipo.getDimensiones().addAll(Tipo.getTipoArray().getDimensiones());
+                    Tipo.setTipoArray(Tipo.getTipoArray().getTipoArray());
+                }
+
+                //Verifico el tipoArray
+                if (Tipo.getTipoArray().getId() != null) {
+                    Simbolo type = e.Get(Tipo.getTipoArray().getId());
+                    if (type == null) {
+                        errores.add(new ErrorC("Semántico", Linea, Columna, "No se ha definido un tipo con el id: " + Tipo.getTipoArray().getId() + "."));
+                        return null;
+                    } else {
+                        if (type.getRol() == Rol.TYPE) {
+
+                            if (type.getTipo().IsArray()) {
+                                Tipo tipSim = type.getTipo();
+
+                                while (tipSim.IsArray()) {
+                                    Tipo.getDimensiones().addAll(tipSim.getDimensiones());
+                                    tipSim = type.getTipo().getTipoArray();
+                                }
+                                Tipo.getTipoArray().setId(Tipo.getTipoArray().getId().toLowerCase());
+                                Tipo.getTipoArray().setTipoPadre(tipSim);
+                            } else {
+                                Tipo.getTipoArray().setId(Tipo.getTipoArray().getId().toLowerCase());
+                                Tipo.getTipoArray().setTipoPadre(type.getTipo());
+                            }
+                        } else {
+                            errores.add(new ErrorC("Semántico", Linea, Columna, Tipo.getTipoArray().getId() + " no es un tipo."));
+                            return null;
+                        }
+                    }
+                } else {
+                    if (Tipo.getTipoArray().getLimiteInf() != null && Tipo.getTipoArray().getLimiteSup() != null) {
+                        Tipo.getTipoArray().getLimiteInf().GetCuadruplos(e, errores);
+                        Tipo.getTipoArray().getLimiteSup().GetCuadruplos(e, errores);
+
+                        if (Tipo.getTipoArray().getLimiteInf().getTipo().IsNumeric() && Tipo.getTipoArray().getLimiteSup().getTipo().IsNumeric()) {
+                            if (Tipo.getTipoArray().getLimiteInf().getTipo().getTipo() == Tipo.getTipoArray().getLimiteSup().getTipo().getTipo()) {
+                                Tipo.getTipoArray().setTipo(Tipo.getTipoArray().getLimiteInf().getTipo().getTipo());
+                            } else {
+                                errores.add(new ErrorC("Semántico", Linea, Columna, "El tipo del límite inferior no coincide con el del límite superior."));
+                                return null;
+                            }
+                        } else {
+                            errores.add(new ErrorC("Semántico", Linea, Columna, "El tipo subrango solo acepta tipos numéricos y carácteres."));
+                            return null;
+                        }
+                    } else if (Tipo.getTipoArray().getVariables() != null) {
+                        Tipo.getTipoArray().setIdRecord(this.hashCode());
+                        Tipo.getTipoArray().setEntorno(new Entorno("record", e));
+                        Tipo.getTipoArray().getVariables().forEach((variable) -> {
+                            variable.GetCuadruplos(Tipo.getTipoArray().getEntorno(), errores, global);
+                        });
+                        Tipo.getTipoArray().getEntorno().setSize(Tipo.getTipoArray().getEntorno().getPos());
+                        Tipo.getTipoArray().getEntorno().setPadre(null);
+                    }
+                }
+
+                for (Dimension dimension : Tipo.getDimensiones()) {
+                    dimension.getLimiteInf().GetCuadruplos(e, errores);
+                    dimension.getLimiteSup().GetCuadruplos(e, errores);
+
+                    if (!dimension.getLimiteInf().getTipo().IsInteger()) {
+                        if (!dimension.getLimiteInf().getTipo().IsChar()) {
+                            errores.add(new ErrorC("Semántico", Linea, Columna, "La dimensión debe ser integer."));
+                            return null;
+                        }
+                    }
+
+                    if (!dimension.getLimiteSup().getTipo().IsInteger()) {
+                        if (!dimension.getLimiteSup().getTipo().IsChar()) {
+                            errores.add(new ErrorC("Semántico", Linea, Columna, "La dimensión debe ser integer."));
+                            return null;
+                        }
+                    }
+
+                }
             }
         }
 
